bc58c4d5b762dd784435174ae7c4c0c17fe249db
Sergey <flargoin@gmail.com>
2023-02-28 02:14:01 +0300

    mod script for slider

--- dist/script.js
+++ dist/script.js
@@ -130,6 +130,18 @@ class Slider {
     if (n < 1) {
       this.slideIndex = this.slides.length;
     }
+    try {
+      this.hanson.style.opacity = '0';
+      if (n === 3) {
+        this.hanson.classList.add('animated');
+        setTimeout(() => {
+          this.hanson.style.opacity = '1';
+          this.hanson.classList.add('slideInUp');
+        }, 3000);
+      } else {
+        this.hanson.classList.remove('slideInUp');
+      }
+    } catch (e) {}
     [...this.slides].forEach(slide => {
       slide.style.display = 'none';
     });
@@ -139,9 +151,17 @@ class Slider {
     this.showSlides(this.slideIndex += n);
   }
   render() {
+    try {
+      this.hanson = document.querySelector('.hanson');
+    } catch (e) {}
     this.btns.forEach(btn => {
       btn.addEventListener('click', () => {
         this.plusSlides(1);
+      });
+      btn.parentNode.previousElementSibling.addEventListener('click', e => {
+        e.preventDefault();
+        this.slideIndex = 1;
+        this.showSlides(this.slideIndex);
       });
     });
     this.showSlides(this.slideIndex);
